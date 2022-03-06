88aa5f49392de4acc7157f7372affe889dcd5933
Sakuma <1217054@ed.tus.ac.jp>
2022-03-06 16:47:51 +0900

    マクロ作成

--- macro_skm.cxx
+++ macro_skm.cxx
@@ -1,13 +1,13 @@
 void macro_skm(TString inputfilename) {
   TFile *file = new TFile(inputfilename, "read");
-  TFile *tree = (TTree*)file->Get("tree");
+  TTree *tree = (TTree*)file->Get("tree");
 
-  Double_t adc[32]
-  tree->SetBranchAddress("adc", &adc);
+  Double_t VadcHigh[64];
+  tree->SetBranchAddress("VadcHigh", &VadcHigh);
 
-  const Int_t N = tin->GetEntries();
-  for (Int_t ienty = 0; ientry < N; ientry++){
-    tree->GetEntry(ienty);
-    cout<<ientry<<"  "<<adc[30]<<endl;
+  const Int_t N = tree->GetEntries();
+  for (Int_t ientry = 0; ientry < N; ientry++){
+    tree->GetEntry(ientry);
+    cout<<ientry<<"  "<<VadcHigh[44]<<endl;
   }
 }
