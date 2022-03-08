812a8008a3fb007178bd06cbe2710f8f09c7d071
Sakuma <1217054@ed.tus.ac.jp>
2022-03-08 14:35:11 +0900

    a

--- data/png2mp4.py
+++ data/png2mp4.py
@@ -2,15 +2,12 @@ import cv2
 import glob
 import shutil
 import os
-<<<<<<< HEAD:data/sokutei_2/png2mp4.py
-=======
 
 print("測定データフォルダ:")#sokutei_5　とか
 data_dir = input()
 #print(data_dir)
 os.chdir(data_dir)
 os.makedirs("fig", exist_ok=True)
->>>>>>> 5c016e6701666063fcd05a01e0ae1908c48090be:data/png2mp4.py
 
 size=(640,480)#サイズ指定
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')#保存形式
@@ -33,15 +30,6 @@ print("保存しました")
 
 save.release() # ファイルを閉じる
 
-<<<<<<< HEAD:data/sokutei_2/png2mp4.py
-print("測定データフォルダ:")
-new_dir = input()
-print(new_dir)
-os.makedirs("%s" % new_dir, exist_ok=True)
-=======
-list1 = os.listdir()
-list_png = [s for s in list1 if ((".PNG") in s) or ((".png") in s)]
 #print(list_png)
-for i in range(len(list_png)):
+#for i in range(len(list_png)):
     #shutil.move(list_png[i], "fig/%s" % list_png[i])
->>>>>>> 5c016e6701666063fcd05a01e0ae1908c48090be:data/png2mp4.py
