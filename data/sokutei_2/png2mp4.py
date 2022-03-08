f0aa7d7b914a762fa2635d67e67e87cf231fdde1
Sakuma <1217054@ed.tus.ac.jp>
2022-03-08 11:55:52 +0900

    al02

--- data/sokutei_2/png2mp4.py
+++ data/sokutei_2/png2mp4.py
@@ -3,8 +3,8 @@ import glob
 
 size=(640,480)#サイズ指定
 fourcc = cv2.VideoWriter_fourcc('m','p','4','v')#保存形式
-str = "sokuteitest"
-save = cv2.VideoWriter(str + '.mp4',fourcc,1.0,size)#動画を保存する形を作成
+str = "sokutei_"
+save = cv2.VideoWriter(str + '.mp4',fourcc,5.0,size)#動画を保存する形を作成
 
 print("保存中...")
 
@@ -15,7 +15,7 @@ for i in range(len(pic_data)):#n回繰り返す
     img=cv2.imread(img)#画像を読み込む
     img=cv2.resize(img,(640,480))#上でサイズを指定していますが、念のため
     save.write(img)#保存
-
+print(len(pic_data))
 print("保存しました")
 
 save.release() # ファイルを閉じる
