b4b4eafa12477205e61356d461484a0146561062
Sakuma <1217054@ed.tus.ac.jp>
2022-03-09 12:24:45 +0900

    積分のプロットをforで．画像で保存できるように

--- al02.cxx
+++ al02.cxx
@@ -50,16 +50,16 @@ Double_t EnergyPhoton(Int_t NumP){
 
 void al02(){
   //gROOT->SetBatch(1);
-  Int_t N =20 ; //測定データ数
-  Double_t start_pos = 60.0;//スタート位置
-  Double_t step = 1.2;//刻み幅
+  Int_t N =2100 ; //測定データ数
+  Double_t start_pos = 30.0;//スタート位置
+  Double_t step = 0.04;//刻み幅
   vector<Double_t> v={};
   vector<Double_t> Num={};
   vector<Double_t> Energy={};
   vector<Double_t> x={};
 
   for (Int_t i = 1; i <= N; i++){
-    TString filename = Form("data/sokutei_16/sokutei_%d.root",i);
+    TString filename = Form("data/sokutei_18/sokutei_%d.root",i);
     Double_t para = GetPara(filename);
     if (para > 0){
       v.push_back(para);
--- slittheo.cxx
+++ slittheo.cxx
@@ -1,37 +1,58 @@
-void slittheo(){
+void plot_integral(Double_t slit_width, Int_t step){
     Double_t a, b, k, R, I0;
 
     a = 0.04 ;
     b = 0.02 ;
     k = 2*TMath::Pi()/(5.2*0.0001);
     R = 295;
-    
+
     TF1 *theo = new TF1("theo","(sin([0]*x)*cos([1]*x)/x)**2",-10,10);
     //TF1 *theo = new TF1("theo","(cos([1]*x))**2",-2,2);  //理論曲線、単スリット幅ゼロ近似
     theo->SetParameter(0,k*b/2/R);
     theo->SetParameter(1,k*a/2/R);
     //theo->Draw();  //理論曲線
 
-    
+
     for(Int_t i = 0; i < 100000; i++){
-        Double_t x = -10 + 2*i*0.0001;cd ..
+        Double_t x = -10 + 2*i*0.0001;
         Double_t deriv = theo->Derivative(x);
         if(-0.00002 < deriv && deriv < 0.00002){
-            cout << x << " " << deriv << endl;  //微分係数が十分小さい時の値を表示
-        } 
+            //cout << x << " " << deriv << endl;  //微分係数が十分小さい時の値を表示
+        }
     }
 
     TGraph *tg = new TGraph();
 
-    for(Int_t i = 0; i < 100000; i++){
-        Double_t x = -10 + 2*i*0.0001;
-        Double_t integ = theo->Integral(x-0.3, x+0.3);
+    TCanvas *c1 = new TCanvas("c1", "c1", 800, 600);
+
+    for(Int_t i = 0; i < 200000; i++){
+        Double_t x = -20 + 2*i*0.0001;
+        Double_t integ = theo->Integral(x-(slit_width/2.0), x+(slit_width/2.0));
         tg->SetPoint(i, x, integ);
     }
+    //Hist01->GetYaxis()->SetRangeUser(0,1200);
+    //Hist01->GetXaxis()->SetRangeUser(500,3500);
+    //Hist01->Draw();
+    TString inputfilename = "slittheo_fig/fig";
+    //inputfilename.ReplaceAll(".root","_Ene.png");
+    TString figname = Form("%s_%d.png",inputfilename.Data(),step);
 
-    tg->Draw("AP");  //MPPC幅を考慮した理論曲線
+    tg -> Draw("AP");  //MPPC幅を考慮した理論曲線
+    //tg->GetYaxis()->SetRangeUser(0,0.2);
+    tg->GetXaxis()->SetRangeUser(-20,20);
 
+    c1 -> SaveAs(figname);
 }
 
 //mm単位
 //数値計算
+
+void slittheo(){
+  Double_t slit_width = 0.1;
+  for(Int_t i = 0; i < 10; i++){
+    Int_t step = 1;
+    cout << slit_width << endl;
+    plot_integral(slit_width, i);
+    slit_width += 0.1;
+  }
+}
